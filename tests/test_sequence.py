1d186a1e657b96902353acce1891db8886ba4b18
Jorge Arevalo <jorge.arevalo@ontruck.com>
2020-01-31 15:34:16 +0100

    More test simplification

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -44,14 +44,14 @@ def test_clean_nones_error():
         clean_nones(1)
 
 
-def test_remove_duplicates_from_list_without_duplicates(list_without_duplicates):
-    assert list_without_duplicates == remove_duplicates(list_without_duplicates)
+@pytest_parametrize_plus("expected_list, list_to_test", [
+    (fixture_ref(list_without_duplicates), fixture_ref(list_without_duplicates)),
+    (fixture_ref(list_without_duplicates), fixture_ref(list_with_duplicates))
+])
+def test_remove_duplicates(expected_list, list_to_test):
+    assert expected_list == remove_duplicates(list_to_test)
 
 
-def test_remove_duplicates_from_list_with_duplicates(list_without_duplicates, list_with_duplicates):
-    assert list_without_duplicates == remove_duplicates(list_with_duplicates)
-
-
-def test_remove_duplicates_from_something_that_is_not_a_list():
+def test_remove_duplicates_errors():
     with pytest.raises(TypeError):
         remove_duplicates(1)
