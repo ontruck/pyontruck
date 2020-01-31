43616e205abbbbc90efd332146459750e553de65
Jorge Arevalo <jorge.arevalo@ontruck.com>
2020-01-31 15:28:34 +0100

    Simplify tests using parametrize

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -5,6 +5,7 @@ test_pyontruck
 Tests for `pyontruck.sequence` module.
 """
 import pytest
+from pytest_cases import pytest_parametrize_plus, fixture_ref
 from pyontruck.sequence import remove_duplicates, clean_nones
 
 
@@ -23,19 +24,22 @@ def none_list():
     return [None, None, None]
 
 
-def test_clean_nones_from_list_without_nones(list_without_duplicates):
-    assert list_without_duplicates == clean_nones(list_without_duplicates)
+@pytest.fixture
+def list_without_duplicates_and_nones(list_without_duplicates, none_list):
+    return list_without_duplicates + none_list
 
 
-def test_clean_nones_from_none_list(none_list):
-    assert [] == clean_nones(none_list)
+# If you want to use fixtures as parametrize arguments, need pytest_cases extension
+@pytest_parametrize_plus("expected_list, list_to_test", [
+    (fixture_ref(list_without_duplicates), fixture_ref(list_without_duplicates)),
+    ([], fixture_ref(none_list)),
+    (fixture_ref(list_without_duplicates), fixture_ref(list_without_duplicates_and_nones))
+])
+def test_clean_nones(expected_list, list_to_test):
+    assert expected_list == clean_nones(list_to_test)
 
 
-def test_clean_nones_from_mixed_list(none_list, list_without_duplicates):
-    assert list_without_duplicates == clean_nones(list_without_duplicates + none_list)
-
-
-def test_clean_nones_from_something_that_is_not_a_list():
+def test_clean_nones_error():
     with pytest.raises(TypeError):
         clean_nones(1)
 
--- tests/test_timing.py
+++ tests/test_timing.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from freezegun import freeze_time
-from pyontruck.timing import get_next_monday
+import pytest
+
+from pyontruck.timing import get_next_monday, get_naive_time_utc
 
 
 @freeze_time('2020-01-30')
@@ -13,3 +15,13 @@ def test_get_next_monday_from_specific_date():
     base_date = datetime(1980, 4, 29)
     next_monday = get_next_monday(base_date)
     assert "1980-05-05" == next_monday.strftime("%Y-%m-%d")
+
+
+def test_naive_time_utc_invalid_country_code():
+    with pytest.raises(KeyError):
+        get_naive_time_utc(datetime.now(), country_code='XX')
+
+
+def test_naive_time_utc_invalid_datetime_value():
+    with pytest.raises(AttributeError):
+        get_naive_time_utc(1, country_code='ES')
